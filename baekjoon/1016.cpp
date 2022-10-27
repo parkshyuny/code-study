ca268f9a4e2cc815684b48fb86c740b8337d736c
Lina Park <100692678+parkshyuny@users.noreply.github.com>
2022-10-27 17:18:23 +0900

    Update 1016.cpp

--- baekjoon/1016.cpp
+++ baekjoon/1016.cpp
@@ -1,5 +1,3 @@
-#Problem: https://www.acmicpc.net/problem/1016
-
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -9,44 +7,39 @@ int sol; // 정답
 
 void InputData()
 {
-	cin >> low;
-	cin >> high;
+        cin >> low;
+        cin >> high;
 }
 void OutputData()
 {
-	cout << sol << "\n";
+        cout << sol << "\n";
 }
 void Solve() {
-	int divs[high + 10];
-	for (int i = 2; i < sqrt(high); i++) {
-		int base = pow(i, 2);
-		int idx = 1;
+        int divs[high + 10];
+        for (int i = 1; i <= high; i++) divs[i] = 0;
 
-		while () {
-			
-		}
-	}
+        for (int i = 2; i <= sqrt(high); i++) {
+                int base = pow(i, 2);
+                int prod = 1;
 
-	int cnt = 0;
-	for (int i = low; i <= high; i++) {
-		if (!divs[i]) ++cnt;
-	}
-	sol = cnt;
+                while (base * prod <= high) {
+                        divs[base * prod] = 1;
+                        prod++;
+                }
+        }
+
+        for (int i = low; i <= high; i++) {
+                if (!divs[i]) ++sol;
+        }
 }
 int main()
 {
 
-	InputData(); // 입력
+        InputData(); // 입력
 
-	// 코드를 작성하세요
-	Solve();
+        // 코드를 작성하세요
+        Solve();
 
-	OutputData(); // 출력
-	return 0;
+        OutputData(); // 출력
+        return 0;
 }
-
-1 2 3 5 6 7 10 11 13 14 15 17 19
-4 8 12 14 16 18 20 24 25 
-
-4: 4 8 12 16 ...
-9: 9 18 ....
